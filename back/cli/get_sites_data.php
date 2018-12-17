02909596a37c1073b83c62e613797ab5a2a82471
Deekor <deekor666@gmail.com>
2018-12-17 02:59:35 +0000

    path editing2

--- back/cli/get_sites_data.php
+++ back/cli/get_sites_data.php
@@ -6,7 +6,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
 )));
 require_once 'config.php';
 require_once 'General.php';
-require_once (SITE_ROOT  . '../public/libs/Smarty.class.php');
+require_once (WWW_DIR . '/libs/Smarty.class.php');
 
 
 $SMARTY = new Smarty();
@@ -14,7 +14,7 @@ $SMARTY->template_dir = 'tpl/';
 $SMARTY->compile_dir = 'tpl_c/';
 $SMARTY->cache_dir = 'cache/';
 General::init($DB, $SMARTY);
-require_once(SITE_ROOT . '../back/controller/SiteController.php');
-require_once(SITE_ROOT . '../back/controller/ParserController.php');
+require_once(BACK_DIR . '/controller/SiteController.php');
+require_once(BACK_DIR . '/controller/ParserController.php');
 
 ParserController::loadSitesData();
